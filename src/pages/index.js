1345b30bde6996be608d3f3ee3bedca37c99944e
Vittawat <vittawat@Vittawats-MacBook-Pro-3.local>
2020-04-06 23:52:55 +0700

    add image2

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => (
     <Link to="page-2" className="gasf"> aaa </Link>
     <p>Bangkok based designer, proficient in product design, interior design, lighting object, 3D Making, furniture, decorative product, sound design. With 4 years of experience in end to end Industrial design, I’ve developed and maintain in organisation’s design process and delivery. Created and executed design projects for Lightsculptures, Heineken, non-corporate clients and etc. Delivered creativity, flexibility and sincerity. Transforming conceptual into rich, fulfilling experiences into completed outcome.</p>
 
-    <Link to="page-2" className="gasf"><img src="../images/IMG_0996.png/></Link>
+    <Link to="page-2" className="gasf"><img src="../images/IMG_0996.png"/></Link>
 
     <img src="../images/IMG_0996.png"/>
     <p>Pavilion</p>
