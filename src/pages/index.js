9aa9fa188dea9405b878c5a6115f75c9a14be3c7
Vittawat <vittawat@Vittawats-MacBook-Pro-3.local>
2020-04-06 23:41:55 +0700

    asd

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,8 +4,7 @@ import { Link } from "gatsby"
 
 import "../components/style.css"
 import Header from "../components/header"
-import Pavilion from "../images/IMG_0996.png"
-import Image from "../images/gatsby-icon.png"
+
 
 const IndexPage = () => (
 
@@ -16,12 +15,12 @@ const IndexPage = () => (
     <Link to="page-2" className="gasf"> aaa </Link>
     <p>Bangkok based designer, proficient in product design, interior design, lighting object, 3D Making, furniture, decorative product, sound design. With 4 years of experience in end to end Industrial design, I’ve developed and maintain in organisation’s design process and delivery. Created and executed design projects for Lightsculptures, Heineken, non-corporate clients and etc. Delivered creativity, flexibility and sincerity. Transforming conceptual into rich, fulfilling experiences into completed outcome.</p>
 
-    <Link to="page-2" className="gasf"><img src={Pavilion}/></Link>
 
 
-    <img src={Pavilion}/>    
+
+    
     <p>Pavilion</p>
-    <img src={Image}/>
+   
     <p>Gatsbyicon</p>
 
   </div>
